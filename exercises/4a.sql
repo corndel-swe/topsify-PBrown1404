d77dacdb6c50139d8b272a557b21c84e1c485ccf
Pbrown1404 <peterbrown140406@gmail.com>
2025-10-06 14:42:43 +0100

    Merged with CLI

--- exercises/4a.sql
+++ exercises/4a.sql
@@ -1,4 +1,4 @@
 -- List the name of every album in the database
 -- Include the artist name
 -- (Make sure to select the album name first, then the artist name)
-SELECT name FROM albums,artists;
+SELECT albums.name,artists.name FROM albums,artists INNER JOIN albums ON artists.id = albums.artists_id;
