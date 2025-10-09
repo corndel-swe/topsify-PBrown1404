f9a15381c3dd675fd5fa872201c559e367b80701
SoftwareEngineer101 <tanianazarian125@gmail.com>
2025-10-09 15:13:56 +0100

    Workshop Complete

---
+++ queries/playlist_create.sql
@@ -0,0 +1,7 @@
+--sqlite3 db/db.sqlite <queries/playlist_create.sql > reports/playlist_create.txt
+
+
+INSERT INTO playlist (user_id, name)
+VALUES
+( '1', 'TechnoEngineering');
+
---
+++ queries/tracks_add.sql
@@ -0,0 +1,6 @@
+--sqlite3 db/db.sqlite <queries/tracks_add.sql > reports/tracks_add.txt
+
+INSERT INTO playlist_tracks (playlist_id, track_id)
+VALUES
+('1', '1MpCaOeUWhox2Fgigbe1cL'),
+('1', '0mKGwFMHzTprtS2vpR3b6s');
